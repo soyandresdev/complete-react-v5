9ee7150f73c06377afa9ef0123c6cb64e2430498
Andres Hernandez Lozano <soyandresdev@gmail.com>
2019-11-12 01:54:52 -0700

    [ADD] Portal and UseEffect Unmount function

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,13 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
+import { navigate } from "@reach/router";
+import Modal from "./Modal";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import ThemeContext from "./ThemeContext";
 
 class Details extends React.Component {
-  state = { loading: true };
+  state = { loading: true, showModal: false };
 
   componentDidMount() {
     pet
@@ -18,20 +20,45 @@ class Details extends React.Component {
           description: animal.description,
           media: animal.photos,
           breed: animal.breeds.primary,
+          url: animal.url,
           loading: false
         });
       })
       .catch(err => this.setState({ error: err }));
   }
+
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+
+  adopt = () => navigate(this.state.url);
+
   render() {
     if (this.state.loading) {
       return <h1>loading … </h1>;
     }
 
-    const { animal, breed, location, description, media, name } = this.state;
+    const {
+      media,
+      animal,
+      breed,
+      location,
+      description,
+      name,
+      showModal
+    } = this.state;
 
     return (
       <div className="details">
+        {showModal ? (
+          <Modal>
+            <div>
+              <h1>Would you like to adopt {name}?</h1>
+              <div className="buttons">
+                <button onClick={this.adopt}>Yes</button>
+                <button onClick={this.toggleModal}>No</button>
+              </div>
+            </div>
+          </Modal>
+        ) : null}
         <Carousel media={media} />
         <div>
           <h1>{name}</h1>
@@ -40,7 +67,7 @@ class Details extends React.Component {
             {([theme]) => (
               <button
                 style={{ backgroundColor: theme }}
-                // onClick={this.toggleModal}
+                onClick={this.toggleModal}
               >
                 Adopt {name}
               </button>
